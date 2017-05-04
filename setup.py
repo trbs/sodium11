bd4b735a57f47e2e4ae5c54e868a2a4575f67dd8
trbs <trbs@trbs.net>
2017-05-04 15:53:00 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name="sodium11",
-    version="0.8.4",
+    version="0.8.5",
     py_modules=['sodium11'],
     install_requires=[
         "PyNaCl>=1.1.2",
